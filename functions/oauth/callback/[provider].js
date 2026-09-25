3260d0c4fdc2bfe95409bb7e393023d39ad9b726
marlonnanico <marlonnanico2@gmail.com>
2026-09-25 16:24:06 -0300

    Update [provider].js

--- functions/oauth/callback/[provider].js
+++ functions/oauth/callback/[provider].js
@@ -1,6 +1,7 @@
 export async function onRequestGet(context) {
   return Response.json({
     callback: true,
-    provider: context.params.provider
+    provider: context.params.provider,
+    url: context.request.url
   });
 }
