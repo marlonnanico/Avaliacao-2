0469baee54ae822229b620cc839de870245313a9
marlonnanico <marlonnanico2@gmail.com>
2026-09-25 16:26:41 -0300

    Update callback response to plain text message

--- functions/oauth/callback/[provider].js
+++ functions/oauth/callback/[provider].js
@@ -1,7 +1,11 @@
 export async function onRequestGet(context) {
-  return Response.json({
-    callback: true,
-    provider: context.params.provider,
-    url: context.request.url
-  });
+
+  return new Response(
+    "CALLBACK FUNCIONANDO",
+    {
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    }
+  );
 }
